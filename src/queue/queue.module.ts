736f9688621a4877bccab8eab06b4f0ceae1815e
AeryIce <agahariiswarajati@gmail.com>
2025-10-10 09:36:17 +0700

    fix: queue module

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { QueueController } from './queue.controller';
-import { PrismaService } from '../prisma.service';
+import { PrismaModule } from '../prisma.module';
 
 @Module({
+  imports: [PrismaModule],           // pakai Prisma dari Global module
   controllers: [QueueController],
-  providers: [QueueService, PrismaService], // ⬅️ tambahkan PrismaService
+  providers: [QueueService],         // cukup QueueService
+  exports: [QueueService],           // <-- PENTING: agar terlihat di LegacyModule
 })
 export class QueueModule {}
